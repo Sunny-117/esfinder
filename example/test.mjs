ca0cf7a82d7343e54b629bdbd03e96b9a38b7220
Sunny-117 <zhiqiangfu6@gmail.com>
2025-02-21 12:58:13 +0800

    test: dynamic import test case

--- example/test.mjs
+++ example/test.mjs
@@ -2,6 +2,7 @@ import { parseExports, getRelatedFiles } from 'esfinder'
 import path from 'node:path'
 
 const files = ['./src/a.js', './src/c.js']
+// const files = ['./src/a.js', './src/c.js', './src/d'] // TODO: 不加后缀
 const importsDir = './src/__tests__'
 
 // 预缓存目标文件的导出内容
